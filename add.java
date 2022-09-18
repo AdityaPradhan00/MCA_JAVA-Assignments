d29a3adeab7d8d0f9806d7ed85d48a1d094a1fc1
Aditya Pradhan <aditya1pradhan21@gmail.com>
2022-09-18 17:21:51 +0530

    added comments

--- add.java
+++ add.java
@@ -1,6 +1,8 @@
+//Q. Write a program to accept two itegers through command line arguments and display the addition,
+//substraction, multiplication and division
 public class add{
     public static void main(String args[]){
-        System.out.print("Sum: " + Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+        System.out.print("Sum: " + (Integer.parseInt(args[0])+Integer.parseInt(args[1])));//pasrsing the string numbers to integer 
         System.out.print("\nDifference: " + (Integer.parseInt(args[0])-Integer.parseInt(args[1])));
         System.out.print("\nProduct: " + Integer.parseInt(args[0])*Integer.parseInt(args[1]));
         System.out.print("\nQuotient: " + Float.parseFloat(args[0])/Float.parseFloat(args[1]));
--- nNumber.java
+++ nNumber.java
@@ -1,7 +1,9 @@
+//Q. Write a program that accepts n-number of integers through command 
+//line arguments and display only the even numbers out of it.
 public class nNumber {
     public static void main(String args[]){
-        int a = Integer.parseInt(args[0]);
-        for(int i = 0; i <= a; i++){
+        int a = Integer.parseInt(args[0]);//parsing the n-number into integer 
+        for(int i = 0; i <= a; i++){//sorting out even numbers 
             if ( i % 2== 0){
                 System.out.print(i + ",");
             }
