8aca658b87319029b51db693bd001c15865b538f
Aditya Pradhan <aditya1pradhan21@gmail.com>
2022-10-04 19:09:54 +0530

    ini commit

---
+++ OddEven.java
@@ -0,0 +1,33 @@
+import java.io.*;
+class Prog
+{
+    int num;
+    void getEven(){
+        System.out.print("Even: ");
+        for(int i = 0; i <= num; i++){//sorting out even numbers 
+            if ( i % 2== 0){
+                System.out.print(i + ",");
+            }
+        }
+    }
+    void getOdd(){
+        System.out.print("\n\nOdd: ");
+        for(int i = 0; i <= num; i++){//sorting out odd numbers 
+            if ( i % 2 != 0){
+                System.out.print(i + ",");
+            }
+        }
+    }
+
+}
+class OddEven{
+    public static void main(String arg[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Prog obj = new Prog();//creating constructor
+        System.out.print("Enter the Range: ");
+        obj.num = Integer.parseInt(br.readLine());
+        obj.getEven();//calling getEven function
+        obj.getOdd();
+       // obj.getPrime();
+    }
+}
